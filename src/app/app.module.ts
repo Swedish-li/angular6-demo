2ffa71a53087672102554976c8f86a54a5fb0b51
Swedish-li <one.piece.li.2016@gmail.com>
2018-08-25 23:13:13 +0800

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AnimationComponent } from './page/animation/animation.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, ja_JP } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
+import { MenuComponent } from './component/menu/menu.component';
+import { AppRoutingModule } from './router/app-routing.module';
+import { AnimationNgComponent } from './page/animation/animation-ng/animation-ng.component';
+import { AnimationCss3Component } from './page/animation/animation-css3/animation-css3.component';
 
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnimationComponent
+    MenuComponent,
+    AnimationNgComponent,
+    AnimationCss3Component
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    AppRoutingModule
   ],
   providers: [{ provide: NZ_I18N, useValue: ja_JP }],
   bootstrap: [AppComponent]
---
+++ src/app/page/animation/animation-css3/animation-css3.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animation-css3',
+  templateUrl: './animation-css3.component.html',
+  styleUrls: ['./animation-css3.component.scss'],
+})
+export class AnimationCss3Component implements OnInit {
+
+  btnState: string;
+  show: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
