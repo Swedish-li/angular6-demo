aca36923a73f67a41c8e63c948d748df3b67b8ca
Swedish-li <one.piece.li.2016@gmail.com>
2018-08-27 14:41:17 +0900

    conflicts 修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-=======
-import { AnimationComponent } from './page/animation/animation.component';
->>>>>>> master
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, ja_JP } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
-<<<<<<< HEAD
+import ja from '@angular/common/locales/ja';
 import { MenuComponent } from './component/menu/menu.component';
 import { AppRoutingModule } from './router/app-routing.module';
 import { AnimationNgComponent } from './page/animation/animation-ng/animation-ng.component';
 import { AnimationCss3Component } from './page/animation/animation-css3/animation-css3.component';
-=======
->>>>>>> master
 
-registerLocaleData(zh);
+registerLocaleData(ja);
 
 @NgModule({
   declarations: [
@@ -34,12 +27,8 @@ registerLocaleData(zh);
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-<<<<<<< HEAD
     NgZorroAntdModule,
     AppRoutingModule
-=======
-    NgZorroAntdModule
->>>>>>> master
   ],
   providers: [{ provide: NZ_I18N, useValue: ja_JP }],
   bootstrap: [AppComponent]
