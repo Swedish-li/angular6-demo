68e0e27dcf5abcdd25124e8e4e92c0ecd6ddf139
Swedish-li <one.piece.li.2016@gmail.com>
2021-05-15 19:49:37 +0900

    update nginx config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular6-demo!');
+    expect(page.getParagraphText()).toEqual('angular demo');
   });
 });
